67a656280b1a34b70cd41fd26e2328c5b4cb90d3
NyanCoder <60844713+fgrammatico@users.noreply.github.com>
2020-03-22 10:23:30 +0000

    added full alphabet

--- l337_translator.py
+++ l337_translator.py
@@ -1,4 +1,6 @@
-intab = 'aeio'
-outtab = '4310'
+#!/usr/bin/env python3
+
+intab = 'qwertyuiopasdfghjklzxcvbnm'
+outtab = 'qw3r7yu10p45df6hjklzxcvbnm'
 s = input ('Enter your password:')
 print(s.translate({ord(x): y for (x, y) in zip(intab, outtab)}))
