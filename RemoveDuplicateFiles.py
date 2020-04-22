49e3a811330851b384700cba12a87aad0b72130a
fgrammatico <fabiogrammatico+github@pm.me>
2020-04-22 11:23:45 +0200

    Input is now a dynamic variable from terminal

--- RemoveDuplicateFiles.py
+++ RemoveDuplicateFiles.py
@@ -1,8 +1,10 @@
+# This script will scan a given folder for audio files with the same name. If a flac/mp3 pair is found , the mp3 will be deleted.
+
 import os
 import logging
 from os.path import splitext
 from collections import Counter
-path=os.getcwd().split(os.sep)[-1]
+path= input ('Enter the full path to scan for duplicate files:')
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s')
 #logging.disable(logging.CRITICAL)
 logging.debug('Start of script')
@@ -23,7 +25,8 @@ def compare_text_flac_mp3(path):
                 logging.debug('Deleting file ' + path + '/' + k + '.mp3')
                 os.remove(path + '/' + k + '.mp3')
         else:
-            print ('nothing to delete')
+            print ('Nothing to delete')
 
 compare_text_flac_mp3(path)
 logging.debug('End of script')
+print (input ('Press any key to close the program'))
