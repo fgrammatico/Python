3a61524959d297bb6c04a9a90fd6928822731071
fgrammatico <fabiogrammatico+github@pm.me>
2020-04-22 21:20:25 +0200

    added id3 tags

--- MyMusic_walker_MySQL_Update.py
+++ MyMusic_walker_MySQL_Update.py
@@ -6,6 +6,10 @@ import mysql.connector
 import os
 import sys
 import time
+from eyed3 import id3
+
+musicfolder='''/home/slashviper/Music/here/'''
+logspath='''/home/slashviper/Music/'''
 
 def humanize_time(secs):
     mins, secs = divmod(secs, 60)
@@ -13,9 +17,9 @@ def humanize_time(secs):
     return '%02d:%02d:%02d' % (hours, mins, secs)
 startTime = time.time()
 # put here any extension to exlude in the loop
-excludeList = [".txt", ".db", ".cache", ".tmp"]
+excludeList = [".txt", ".db", ".cache", ".tmp", ".log"]
 
-sourcepath = "/mnt/raid1/Plex/Music/Archive/"
+sourcepath = musicfolder
 mydb = mysql.connector.connect(
                                host="localhost",
                                user="root",
@@ -24,19 +28,22 @@ mydb = mysql.connector.connect(
                                )
 mycursor = mydb.cursor()
 recordCount = skipCount = dupCount = 0
-logName = "/mnt/raid1/MariaDB/logs" + time.strftime("%Y%m%d%H%M%S", time.localtime(time.time())) + ".log"
-dupLog = "/mnt/raid1/MariaDB/logs" + time.strftime("%Y%m%d%H%M%S", time.localtime(time.time())) + "_duplicate.log"
+logName = logspath + time.strftime("%Y%m%d%H%M%S", time.localtime(time.time())) + ".log"
+dupLog = logspath + time.strftime("%Y%m%d%H%M%S", time.localtime(time.time())) + "_duplicate.log"
 logFile = open(logName, "w")
 dupFile = open(dupLog, "w")
 sql = "INSERT INTO `MyMusic`(`Name`,`Artist`,`Song`,`Path`,`Extension`, `Requested`) VALUES (%s,%s,%s,%s,%s,%s);"
-try:
-    for path, subdirs, files in os.walk(sourcepath):
+for path, subdirs, files in os.walk(sourcepath):
         for name in files:
             fullname = file = os.path.join(sourcepath, name)
+            name = os.path.splitext(file)[0]
             extension = os.path.splitext(file)[1]
+            tag = id3.Tag()
+            tag.parse(fullname)
+            artist = tag.artist
+            song = tag.title
+            requested = 'yes'
             sanitazedName = path
-            #sanitazedName = fullname.replace("gluster", "\\vodstp01-smb.svc.dmc-int.net")
-            #sanitazedName = sanitazedName.replace(os.sep, '\\')
             cSql = "SELECT COUNT(*) FROM MyMusic where Name=%s"
             cVal = (path,)
             mycursor.execute(cSql, cVal)
@@ -58,8 +65,6 @@ try:
                 recordCount += 1
             sys.stdout.write("\r" + time.strftime("%H:%M:%S", time.localtime(time.time())) + ' -> Record Inserted. Total Inserted: ' + str(recordCount) + ' Skipped: ' + str(skipCount) + ' (Duplicate: ' + str(dupCount) + ') in: ' + humanize_time(time.time() - startTime))
             sys.stdout.flush()
-except:
-    print("An exception occurred")
 duration = time.time()-startTime
 print ("\nJob Completed\n" + str(recordCount) + ' records inserted\n' + str(skipCount) + ' records skipped (Duplicate: ' + dupCount + ')\nDuration: ' + humanize_time(duration) + ' seconds')
 logFile.close()
