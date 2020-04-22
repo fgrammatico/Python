e36e867b590e0e6cf434a2d99d06dc10dac7fd61
fgrammatico <fabiogrammatico+github@pm.me>
2020-04-22 11:01:25 +0200

    fixed bug with entering a path without /

--- RemoveDuplicateFiles.py
+++ RemoveDuplicateFiles.py
@@ -2,6 +2,7 @@ import os
 import logging
 from os.path import splitext
 from collections import Counter
+path=os.getcwd().split(os.sep)[-1]
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s')
 #logging.disable(logging.CRITICAL)
 logging.debug('Start of script')
@@ -9,7 +10,7 @@ logging.debug('Start of script')
 def compare_text_flac_mp3(path):
     #List containing all file names + their extension in path directory
     myDir = os.listdir(path)
-    logging.debug('The list of folders is' + str(myDir))
+    #logging.debug('The list of folders is' + str(myDir))
     #List containing all file names without their extension
     l = [splitext(filename)[0] for filename in myDir]
 
@@ -17,13 +18,12 @@ def compare_text_flac_mp3(path):
     a = dict(Counter(l))
     #Loop files name that have same name and different extension
     for k,v in a.items():
-        if v > 1:
-            if os.path.exists(path + k + '.mp3'):
-                #If path exist then remove the duplicate file
-                logging.debug('Deleting file ' + k)
-                os.remove(path + k + '.mp3')
-            else:
-                print ('nothing to delete')
-logging.debug('End of script')
+        if v >= 2:
+            if os.path.exists(path + '/' + k + '.mp3'):
+                logging.debug('Deleting file ' + path + '/' + k + '.mp3')
+                os.remove(path + '/' + k + '.mp3')
+        else:
+            print ('nothing to delete')
 
-compare_text_flac_mp3('''/mnt/md-name-H4cKn3t:0/Music/DjPlaylistmix/BigBeat and Breaks/''')
+compare_text_flac_mp3(path)
+logging.debug('End of script')
