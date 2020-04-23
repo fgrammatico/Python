bc6203a8f2d42813123c19ddbb0754ede42e06a0
fgrammatico <fabiogrammatico+github@pm.me>
2020-04-23 10:37:03 +0200

    Corrected fullname with fullpath instead and str dupCount

--- MyMusic_walker_MySQL_Update.py
+++ MyMusic_walker_MySQL_Update.py
@@ -8,8 +8,8 @@ import sys
 import time
 from eyed3 import id3
 
-musicfolder='''/mnt/raid1/Plex/Music/Archive/'''
-logspath='''/mnt/raid1/Plex/Music/'''
+musicfolder='''/home/slashviper/Downloads/test/'''
+logspath='''/media/slashviper/HD_Crap/'''
 
 def humanize_time(secs):
     mins, secs = divmod(secs, 60)
@@ -22,9 +22,9 @@ excludeList = [".txt", ".db", ".cache", ".tmp", ".log"]
 sourcepath = musicfolder
 mydb = mysql.connector.connect(
                                host="localhost",
-                               user="root",
-                               passwd="MySql23",
-                               database="MyMusic"
+                               user="slashviper",
+                               passwd="Xcom23",
+                               database="slashviper"
                                )
 mycursor = mydb.cursor()
 recordCount = skipCount = dupCount = 0
@@ -35,11 +35,12 @@ dupFile = open(dupLog, "w")
 sql = "INSERT INTO `MyMusic`(`Name`,`Artist`,`Song`,`Path`,`Extension`, `Requested`) VALUES (%s,%s,%s,%s,%s,%s);"
 for path, subdirs, files in os.walk(sourcepath):
         for name in files:
-            fullname = file = os.path.join(sourcepath, name)
+            fullpath = path
+            fullname = file = os.path.join(fullpath, name)
             name = os.path.splitext(file)[0]
             extension = os.path.splitext(file)[1]
             tag = id3.Tag()
-            tag.parse(fullname)
+            tag.parse(file)
             artist = tag.artist
             song = tag.title
             requested = 'yes'
@@ -66,5 +67,5 @@ for path, subdirs, files in os.walk(sourcepath):
             sys.stdout.write("\r" + time.strftime("%H:%M:%S", time.localtime(time.time())) + ' -> Record Inserted. Total Inserted: ' + str(recordCount) + ' Skipped: ' + str(skipCount) + ' (Duplicate: ' + str(dupCount) + ') in: ' + humanize_time(time.time() - startTime))
             sys.stdout.flush()
 duration = time.time()-startTime
-print ("\nJob Completed\n" + str(recordCount) + ' records inserted\n' + str(skipCount) + ' records skipped (Duplicate: ' + dupCount + ')\nDuration: ' + humanize_time(duration) + ' seconds')
+print ("\nJob Completed\n" + str(recordCount) + ' records inserted\n' + str(skipCount) + ' records skipped (Duplicate: ' + str(dupCount) + ')\nDuration: ' + humanize_time(duration) + ' seconds')
 logFile.close()
